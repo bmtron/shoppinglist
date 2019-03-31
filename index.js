e76404b6a78d210fcd9d60c2e8ef5de64eda7ec6
Brendan Meehan <bmmeehan3@gmail.com>
2019-03-31 02:07:55 -0400

    semi funtional shopping list

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 function test() {
     $('#js-shopping-list-form').submit(function(event) {
         event.preventDefault();
-        alert('help');
+        //alert('HI');
+
+        let itemA = $('#shopping-list-entry').val();
+        $('.shopping-list').append(`<li>
+        <span class="shopping-item">${itemA}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`);
+      $('#shopping-list-entry').val('');  
     });
 }
+function checkList() {
+    $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+        //$('.shopping-item').toggleClass('shopping-item__checked');
+        
+        $(event.currentTarget).closest("li").toggleClass('shopping-item__checked');
 
+    });
+}
 $(test);
+$(checkList);
